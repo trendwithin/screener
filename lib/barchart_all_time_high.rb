9d88e34c4a1b08feb4ac09ac45bbdc2e260da0bf
Michael Becco <e4e6d4d5@gmail.com>
2016-06-07 12:08:53 -0700

    Add Barchart::AllTimeHigh#extract_tickers and Runner

--- lib/barchart_all_time_high.rb
+++ lib/barchart_all_time_high.rb
@@ -11,6 +11,11 @@ module Barchart
       @mechanize.get(url).search(html_tag)
     end
 
+    def extract_tickers(page)
+      total_symbols = page[6].to_s.scan(/[A-Z]+,[^a-z]+[A-Z]/)
+      tickers = total_symbols[0].split(',')
+    end
+
     def insert_all_time_highs(tickers)
       tickers.each { |t| ::AllTimeHigh.create!(symbol: t, saved_on: Time.now) }
     end
