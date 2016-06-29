01fd8639958c76de346c61cfbc7b217b648bd3ad
Michael Becco <e4e6d4d5@gmail.com>
2016-06-28 17:22:01 -0700

    Add DailyHighLow Helper for HighCharts line_chart and SQL Query for Data

--- app/helpers/daily_high_lows_helper.rb
+++ app/helpers/daily_high_lows_helper.rb
@@ -1,2 +1,17 @@
 module DailyHighLowsHelper
+
+  def high_low_chart
+    results = DailyHighLow.diff
+    diff_data = []
+    results.columns.each_with_index do |category_name, index|
+      category_data = {}
+      results.rows.each do |raw_data_row|
+        category_data[raw_data_row[5]] = raw_data_row[index] unless index == 5
+      end
+      unless category_name == 'saved_on'
+        diff_data << { name: category_name, data:  category_data }
+      end
+    end
+  line_chart diff_data
+  end
 end
