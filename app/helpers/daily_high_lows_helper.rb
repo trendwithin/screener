681911785fa24fb9a5affe8ecdea78d0724f81a6
Michael Becco <e4e6d4d5@gmail.com>
2016-06-28 17:38:21 -0700

    Refactor DailyHighLowsController #index

--- app/helpers/daily_high_lows_helper.rb
+++ app/helpers/daily_high_lows_helper.rb
@@ -1,11 +1,11 @@
 module DailyHighLowsHelper
 
   def high_low_chart
-    results = DailyHighLow.diff
+    # results = DailyHighLow.diff
     diff_data = []
-    results.columns.each_with_index do |category_name, index|
+    @daily_difference.columns.each_with_index do |category_name, index|
       category_data = {}
-      results.rows.each do |raw_data_row|
+      @daily_difference.rows.each do |raw_data_row|
         category_data[raw_data_row[5]] = raw_data_row[index] unless index == 5
       end
       unless category_name == 'saved_on'
