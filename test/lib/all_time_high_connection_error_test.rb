2646a990db620e9ad0217c555015c8a8e532f442
Michael Becco <e4e6d4d5@gmail.com>
2016-06-30 19:32:04 -0700

    Add Error Logging to AllTimeHigh, Refactor Methods, Test Failure

---
+++ test/lib/all_time_high_connection_error_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+require_relative '../../lib/barchart_new_high.rb'
+require 'net/http'
+require 'webmock'
+include WebMock::API
+
+module Barchart
+  class AllTimeHighConnectionErrorTest < ActiveSupport::TestCase
+    def test_all_time_high_connection_errs_out
+      VCR.use_cassette('loading-barchart-all-time-high') do
+        url = "http://www.barchart.com/stocks/athigh.php"
+        html_tag = 'input'
+        @agent = Barchart::AllTimeHigh.new
+        @response = Net::HTTP.get_response(URI(url))
+        stub_request(:paging, url).to_raise('Error').to_raise('Error').to_return(body: 'OK')
+        @agent.paging(url)
+      end
+    end
+  end
+end
