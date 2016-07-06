76918bb5bd69c5a4bab51b710b0710b00663ba65
Michael Becco <e4e6d4d5@gmail.com>
2016-07-06 09:03:47 -0700

    Add NavBar Links/Test and Modify Routes Accordingly

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
-  get 'briefings_earnings/index'
+  get 'briefings', to: 'briefings_earnings#index'
 
-  get 'zacks/index'
+  get 'zacks', to: 'zacks#index'
 
-  get 'new_highs/index'
+  get 'new_highs', to: 'new_highs#index'
 
-  get 'all_time_highs/index'
+  get 'all_time_highs', to: 'all_time_highs#index'
 
-  get 'daily_high_lows/index'
+  get 'daily_high_lows', to: 'daily_high_lows#index'
 
   root 'static_pages#home'
 end
