74407180d87d531723bdea4e777af362d6483e5b
Michael Becco <e4e6d4d5@gmail.com>
2016-07-08 15:35:36 -0700

    Add search feature to zacks.rb and #remove_text, #surprise #reported

--- app/controllers/zacks_controller.rb
+++ app/controllers/zacks_controller.rb
@@ -1,5 +1,8 @@
 class ZacksController < ApplicationController
   def index
     @zacks_earnings = Zack.all.limit(10)
+    @earnings = Zack.remove_text.surprise(params[:query]).reported
   end
+
+  private
 end
